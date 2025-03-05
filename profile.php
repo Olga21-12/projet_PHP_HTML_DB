96458e924d7278f90cbe756c3391f5370e86926d
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-03-05 13:19:50 +0100

    code de page utilisateur ajouté

--- profile.php
+++ profile.php
@@ -1,34 +1,41 @@
 <?php
-/*
 session_start();
 
-if (!isset($_SESSION['user'])) {
+if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
-}*/
+}
+
 $title = "Mon profil";
-$nav = "./profile.php"; 
+$nav = "./profile.php";
+require 'src/config/database.php';
 
 require "./header.php";
+
+$user_id = $_SESSION['user_id'];
+$stmt = $conn->prepare("SELECT pseudo, nom, prenom, date_naissance, description FROM users WHERE id_user = ?");
+$stmt->execute([$user_id]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
 <div class="main-content">
     <div class="container__profil">
-            <h1>Profil (prénom + nom)</h1>
+            <h1><?= htmlspecialchars($user['prenom'] . ' ' . $user['nom']) ?></h1>
             <div class="profil__info">
                 <img src="#" alt="photo" title="photo">
                 <div class="profil__details">
                     <ul>
-                        <li>Pseudo :</li>
-                        <li>Ville :</li>
+                        <li>Pseudo :<?= htmlspecialchars($user['pseudo']) ?></li>
+                        <!--<li>Ville :</li>
                         <li>Nationalite :</li>
                         <li>Date de naissance :</li>
-                        <li>Age :</li>
+                        <li>Age :</li> -->
                     </ul>
                 </div>
             </div>
             <div class="about__me">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni velit distinctio repellat. Quisquam nostrum odio, et nobis obcaecati incidunt amet ipsa accusantium alias error.</p>
+                <p><?= nl2br(htmlspecialchars($user['description'])) ?></p>
             </div>
            
     </div>
